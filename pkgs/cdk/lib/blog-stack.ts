3f6a32da1fa7df73e8ed12090334fb300387df3d
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-06-06 16:47:54 +0900

    update

--- pkgs/cdk/lib/blog-stack.ts
+++ pkgs/cdk/lib/blog-stack.ts
@@ -1,4 +1,3 @@
-import { join } from "node:path";
 import * as cdk from "aws-cdk-lib";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
@@ -8,15 +7,29 @@ import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as lambdaNodejs from "aws-cdk-lib/aws-lambda-nodejs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import type { Construct } from "constructs";
+import { join } from "node:path";
 
+/**
+ * 技術ブログプラットフォーム
+ */
 export class BlogStack extends cdk.Stack {
+  /** テーブル */
   public readonly table: dynamodb.TableV2;
+  /** ユーザープール */
   public readonly userPool: cognito.UserPool;
+  /** ユーザープールクライアント */
   public readonly userPoolClient: cognito.UserPoolClient;
 
+  /**
+   * コンストラクター
+   * @param scope 
+   * @param id 
+   * @param props 
+   */
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // DynamoDBに作成するブログ用のテーブル
     this.table = new dynamodb.TableV2(this, "BlogTable", {
       partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
@@ -31,6 +44,7 @@ export class BlogStack extends cdk.Stack {
       ],
     });
 
+    // ユーザープール
     this.userPool = new cognito.UserPool(this, "BlogUserPool", {
       signInAliases: { email: true },
       selfSignUpEnabled: true,
@@ -45,6 +59,7 @@ export class BlogStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY, // for development
     });
 
+    // ユーザープールクライアント
     this.userPoolClient = new cognito.UserPoolClient(
       this,
       "BlogUserPoolClient",
@@ -57,11 +72,6 @@ export class BlogStack extends cdk.Stack {
         generateSecret: false,
       },
     );
-
-    new cdk.CfnOutput(this, "UserPoolId", { value: this.userPool.userPoolId });
-    new cdk.CfnOutput(this, "UserPoolClientId", {
-      value: this.userPoolClient.userPoolClientId,
-    });
 
     // --- SPA Hosting: S3 + CloudFront (OAC) ---
     const spaBucket = new s3.Bucket(this, "SpaBucket", {
@@ -90,13 +100,9 @@ export class BlogStack extends cdk.Stack {
       ],
     });
 
-    new cdk.CfnOutput(this, "DistributionUrl", {
-      value: `https://${distribution.distributionDomainName}`,
-    });
-
     // --- Lambda API Function ---
     const fn = new lambdaNodejs.NodejsFunction(this, "BlogApiFunction", {
-      runtime: lambda.Runtime.NODEJS_20_X,
+      runtime: lambda.Runtime.NODEJS_24_X,
       entry: join(__dirname, "../../backend/src/index.ts"),
       handler: "handler",
       environment: {
@@ -127,6 +133,20 @@ export class BlogStack extends cdk.Stack {
         allowedHeaders: ["Content-Type", "Authorization"],
         allowCredentials: false,
       },
+    });
+
+    // ======================================================================================
+    // CDKの成果物
+    // ======================================================================================
+
+    new cdk.CfnOutput(this, "UserPoolId", { value: this.userPool.userPoolId });
+    
+    new cdk.CfnOutput(this, "UserPoolClientId", {
+      value: this.userPoolClient.userPoolClientId,
+    });
+
+    new cdk.CfnOutput(this, "DistributionUrl", {
+      value: `https://${distribution.distributionDomainName}`,
     });
 
     new cdk.CfnOutput(this, "ApiUrl", { value: fnUrl.url });
